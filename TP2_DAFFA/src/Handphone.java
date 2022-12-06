e2c2b2bf1ef7225c5a358fb01f3daece74881cb4
daffaabdillahsyah <116296645+daffaabdillahsyah@users.noreply.github.com>
2022-12-06 16:04:57 +0700

    Add files via upload

---
+++ TP2_DAFFA/src/Handphone.java
@@ -0,0 +1,29 @@
+public class Handphone extends Perangkat{
+    protected Boolean fingerprint;
+
+    public Handphone(String drive, int ram, float processor, boolean fingerprint){
+        super(drive, ram, processor);
+        this.fingerprint = fingerprint;
+    }
+
+    public void informasi(){
+        if(fingerprint == true){
+            System.out.println("Handphone ini memiliki drive tipe " + drive + " dengan ram sebesar " + ram +" dan processor " + "secepat " + processor +" Ghz. Selain itu juga laptop ini memiliki Fingerprint");
+        }
+        else {
+            System.out.println("Handphone ini  memiliki drive tipe " + drive + " dengan ram sebesar " + ram +" dan processor " + "secepat " + processor +" Ghz. Selain itu juga laptop ini tidak memiliki Fingerprint");
+
+        }
+    }
+    public void telfon(int no_hp){
+        System.out.println("Handphone berhasil menyambungkan telfon ke No " + no_hp);
+    }
+
+    public void kirimSMS(int no_hp){
+        System.out.println("Handphone berhasul mengirim SMS ke No " + no_hp);
+    }
+
+    public void kirimSMS(int no_hp1, int no_hp2){
+        System.out.println("Handphone berhasil mengirim SMS ke No " + no_hp1 + " dan ke No " + no_hp2);
+    }
+}
